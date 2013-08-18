1fbc7af1cdd5e6ef644e23e47586668d8e4a469a
Flavio Amieiro <amieiro.flavio@gmail.com>
2013-08-18 14:32:56 -0300

    Changes in graphs
    This fixes the y axis limit, and adds labels in both axes.

--- plot_graphs.py
+++ plot_graphs.py
@@ -3,22 +3,45 @@ import os
 import sys
 
 import matplotlib
-matplotlib.use("Agg")
+#matplotlib.use("Agg")
 
 from matplotlib.pyplot import figure
 import numpy
 
-data_files = glob.glob('results/*.dat')
-width = 800
-height = 600
+data_files = glob.glob('results/*/*.dat')
+width = 1280
+height = 720
+DPI = 80
 for data_file in data_files:
-    f = figure(figsize=(width / 80, height / 80), dpi=80)
-    s = f.add_subplot(1, 1, 1)
+    results_dir, project, filename = data_file.split('/')
+    base_filename = filename.rsplit('.', 1)[0]
+    metadata_file = os.path.join(results_dir, project,
+        '{}.metadata'.format(base_filename))
+
+    fig = figure(figsize=(width / DPI, height / DPI), dpi=DPI)
+    subplot = fig.add_subplot(111)
     with open(data_file, 'r') as fp:
-        plot_data = [float(line.strip()) for line in fp.readlines()]
-    if not plot_data:
+        lines = fp.readlines()
+    hist_data = [float(line.strip()) for line in lines]
+
+    with open(metadata_file, 'r') as fp:
+        metadata = fp.read().split(';')
+        tag_name, date = metadata[:2]
+        authors, commits = map(int, metadata[2:])
+
+    if not hist_data:
         sys.stderr.write("File {} had no data.\n".format(data_file))
         continue
-    s.hist(plot_data, bins=numpy.arange(0.00, 1.01, 0.05))
-    project_name = os.path.basename(data_file).rsplit('.dat')[0]
-    f.savefig('results/graphs/{}.png'.format(project_name))
+
+    subplot.hist(hist_data, bins=numpy.arange(0.00, 1.01, 0.05))
+    subplot.set_ylim(0, 500)
+    fig.suptitle('{} - {}'.format(project, tag_name))
+    subplot.set_ylabel('Files')
+    subplot.set_xlabel('Warnings/LOC')
+
+    graphs_dir = 'results/graphs/{}'.format(project)
+
+    if not os.path.exists(graphs_dir):
+        os.mkdir(graphs_dir)
+
+    fig.savefig('{}/{}.png'.format(graphs_dir, base_filename))
